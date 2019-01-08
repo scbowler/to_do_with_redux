923e68c378910722f59a30116ee12f5130c1c997
Scott Bowler <scott.bowler@learningfuze.com>
2019-01-08 15:20:06 -0800

    Completed getting to do items from to do api

---
+++ src/components/list.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getAllItems } from '../actions';
+
+class List extends Component {
+    componentDidMount(){
+        this.props.getAllItems();
+    }
+
+    renderList(){
+        const { todos } = this.props;
+
+        if(!todos){
+            return <h1 className="center">LOADING...</h1>;
+        }
+
+        if(!todos.length){
+            return <h5 className="center">No To Do Items</h5>;
+        }
+
+        const listElements = todos.map(item => {
+            return <li className="collection-item" key={item._id}>{item.title}</li>;
+        });
+
+        return (
+            <ul className="collection">
+                {listElements}
+            </ul>
+        );
+    }
+
+    render(){
+        return (
+            <div>
+                <div className="center">
+                    <h1>To Do List</h1>
+                    <h5 className="grey-text">Now with Redux!</h5>
+                </div>
+                {this.renderList()}
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        todos: state.list.all
+    }
+}
+
+export default connect(mapStateToProps, {
+    getAllItems: getAllItems
+})(List);
--- src/reducers/list_reducer.js
+++ src/reducers/list_reducer.js
@@ -1,12 +1,14 @@
 import types from '../actions/types';
 
 const DEFAULT_STATE = {
-    all: [],
+    all: null,
     single: {}
 };
 
 export default (state = DEFAULT_STATE,  action) => {
     switch(action.type){
+        case types.GET_ALL_TO_DOS:
+            return { ...state, all: action.payload.data.todos };
         default:
             return state;
     }
